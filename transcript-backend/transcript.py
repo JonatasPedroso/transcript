f064c1b189b42a95603a25f1ee9d3e5133cc3b5b
Jonatas Pedroso <jonatasarlindo@hotmail.com>
2024-11-10 22:09:59 -0300

    Atualizando script de transcricao.

--- transcript-backend/transcript.py
+++ transcript-backend/transcript.py
@@ -1,12 +1,13 @@
 import os
+import whisper
+import torch
 from flask import Flask, request, jsonify
-import speech_recognition as sr
-import subprocess
 import tempfile
+import subprocess
 from flask_cors import CORS
+import warnings
 
 app = Flask(__name__)
-
 CORS(app)
 
 def convert_to_wav(file):
@@ -19,21 +20,26 @@ def convert_to_wav(file):
         subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return tmp_filename
 
+def load_whisper_model(model_name, device):
+    model_path = os.path.expanduser(f'~/.cache/whisper/{model_name}.pt')
+    if os.path.exists(model_path):
+        os.remove(model_path)
+        print(f"Deleted corrupted model file: {model_path}")
+    return whisper.load_model(model_name, device=device).to(device)
+
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     if 'audio' not in request.files:
-        return jsonify({"error": "No file part"}), 400
-    
+        return jsonify({"error": "Nenhum arquivo enviado"}), 400
+
     file = request.files['audio']
     if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
+        return jsonify({"error": "Nenhum arquivo selecionado"}), 400
 
     filename = file.filename.lower()
     valid_extensions = ['.opus', '.dat', '.m4a']
-    
-    if any(filename.endswith(ext) for ext in valid_extensions):
-        recognizer = sr.Recognizer()
 
+    if any(filename.endswith(ext) for ext in valid_extensions):
         try:
             with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
                 tmp_filename = tmp_file.name
@@ -41,24 +47,25 @@ def transcribe():
 
             wav_file = convert_to_wav(tmp_filename)
 
-            with sr.AudioFile(wav_file) as source:
-                audio = recognizer.record(source)
-                text = recognizer.recognize_google(audio, language="pt-BR")
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            model = load_whisper_model("large", device)
+
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", UserWarning)
+                result = model.transcribe(wav_file, language="pt", temperature=0.0, word_timestamps=True)
 
             os.remove(tmp_filename)
             os.remove(wav_file)
 
-            return jsonify({"text": text})
+            transcription = result["text"]
 
-        except sr.UnknownValueError:
-            return jsonify({"error": "Could not understand the audio"}), 400
-        except sr.RequestError as e:
-            return jsonify({"error": f"Could not request results; {e}"}), 500
+            return jsonify({"text": transcription})
+
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
     else:
-        return jsonify({"error": "Unsupported file type"}), 400
+        return jsonify({"error": "Tipo de arquivo não suportado"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
